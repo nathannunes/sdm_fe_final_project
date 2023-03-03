79da2425379710c70df1b999af58422a1189939a
azinneck0485 <123660683+azinneck0485@users.noreply.github.com>
2023-03-03 17:40:57 -0500

    AB#87 added logout button and additional formatting

--- sdm_frontend/src/components/NavBar.js
+++ sdm_frontend/src/components/NavBar.js
@@ -2,26 +2,33 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 import './navbar.css';
 
 function NavBar() {
-  return (
+    const logoutHandler = () =>
+    {
+        console.log("logout");
+    }
+
+    return (
         <Navbar className="navbar-custom" 
                 variant="dark"
                 expand="md">
-            <Container>
+
                 <Navbar.Brand to="/">CPT</Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link to="/">Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/courses">Course Catalog</Nav.Link>
                         <Nav.Link href="/schedule">Course Schedule</Nav.Link>
                         <Nav.Link href="/calendar">Academic Calendar</Nav.Link>
+                        <Nav><Button bsPrefix="btn-custom" className="logout" onClick={logoutHandler}>Logout</Button></Nav>
                     </Nav>
                 </Navbar.Collapse>
-            </Container>
         </Navbar>
   );
 }
