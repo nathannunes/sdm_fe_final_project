4a2e4e8a53a440fd371727149ebb4d5ffa0fee13
azinneck0485 <123660683+azinneck0485@users.noreply.github.com>
2023-03-28 20:29:54 -0400

    AB#87 add test for useToken.js

---
+++ sdm_frontend/src/__tests__/useToken.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { describe, expect } from '@jest/globals';
+import { render, screen, act, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import useToken from '../components/useToken';
+
+// render the component for testing
+// this will check that components are rendered as expected
+describe('useToken', () => {
+    it('checks setToken', () => {
+        const useStateMock = jest.spyOn(React, 'useState');
+
+        const setState = jest.fn();
+        useStateMock.mockImplementation(init => [init, setState]);
+
+        global.localStorage.setItem("token","");
+
+        const {setToken} = useToken();
+        
+        const mockToken = {
+            "token": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlcjUiLCJpYXQiOjE2Nzc4NTEyNTcsImV4cCI6MTY3Nzg1MjY5N30.pJWgqDbTyk2EfVHmCUnpRNKIqmot1L2FXI4WrAL363I",
+        };
+
+        setToken(mockToken.token);
+
+        expect(global.sessionStorage.getItem("token")).toContain(mockToken.token);
+
+        global.sessionStorage.clear("token");
+    })
+})
